d12dbba2aa86ce2f85e9e54d6b4ab634e46c52f6
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-11-24 16:45:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ file = pathlib.Path(__file__).parent
 
 README = (file / "README.md").read_text()
 
-# requirements = (file / "requirements.txt").read_text().splitlines()
-
 setup(
     name="pytrycatch",
     version="0.0.1",
     description="Simplified exception handling for Python",
     author="Nuhman PK",
     author_email="nuhmanpk7@gmail.com",
-    long_description = README,
-    long_description_content_type = "text/markdown",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/nuhmanpk/pytrycatch",
-    # packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # install_requires=requirements,
+    install_requires=[
+        'pytest>=7.0',
+        'flake8>=6.0',
+    ],
     python_requires=">=3.9",
     project_urls={
         'Documentation': 'https://github.com/nuhmanpk/pytrycatch/blob/main/README.md',
